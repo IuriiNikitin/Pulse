911711f16898c857a7dffe0de08e1e58ee6721de
Yurii <ykyipka@gmail.com>
2023-04-08 19:22:58 +0300

    add catalog and feed section

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
+"use strict";
 
+// Slider
 
 const slider = tns({
     container: '.carousel__inner',
@@ -21,3 +23,40 @@ document.querySelector(".carousel__prev").addEventListener("click", () => {
 document.querySelector(".carousel__next").addEventListener("click", () => {
     slider.goTo('next');
   });
+
+// Catalog
+
+const catalog = document.querySelector(".catalog"),
+catalogLinks = catalog.querySelectorAll(".catalog-item__link , .catalog-item__back"),
+contentClass = "catalog-item__content",
+listClass = "catalog-item__list";
+
+catalogLinks.forEach(catalogLink => {
+  catalogLink.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const content = e.target.parentNode.parentNode.querySelector("." + contentClass),
+    list = e.target.parentNode.parentNode.querySelector("." + listClass);
+
+    content.classList.toggle(contentClass + "_active");
+    list.classList.toggle(listClass + "_active");
+  });
+});
+
+// CatalogTabs
+
+const tabs = catalog.querySelectorAll(".catalog__tab");
+const content = catalog.querySelectorAll(".catalog__content");
+
+tabs.forEach((tab, i) => {
+  tab.addEventListener("click", () => {
+    for(let j = 0; j < tabs.length; j++) {
+      tabs[j].classList.remove("catalog__tab_active");
+      content[j].classList.remove("catalog__content_active");
+    }
+
+    tabs[i].classList.add("catalog__tab_active");
+    content[i].classList.add("catalog__content_active");
+
+  });
+});
